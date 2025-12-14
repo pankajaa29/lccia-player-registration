2e43e039871af63726e7694215d5fd711d5fbfe9
Pankaj Attarde <pankajaa29@gmail.com>
2025-12-14 17:32:10 +0530

    Fix routing using HashRouter for admin page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import { HashRouter, Routes, Route } from "react-router-dom";
 import PlayerRegistration from "./PlayerRegistration";
+import AdminDashboard from "./AdminDashboard";
 
 function App() {
-  return <PlayerRegistration />;
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<PlayerRegistration />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+      </Routes>
+    </HashRouter>
+  );
 }
 
 export default App;
-
